b1267b22d2467bdc1d925b06a09f26c434f9a6fb
Saju Paul <saju.christel@blu-pa.com>
2019-07-24 20:58:24 +0530

    Invoke currency exchange service from currency conversion service

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,11 +1,15 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
 import com.in28minutes.microservices.currencyconversionservice.bean.CurrencyConversionBean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -16,6 +20,18 @@ public class CurrencyConversionController {
                                                  @PathVariable("quantity") final BigDecimal quantity
     ) {
 
-        return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+        final Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversionBean.class,
+                uriVariables);
+        CurrencyConversionBean response = responseEntity.getBody();
+        return new CurrencyConversionBean(response.getId(), from, to,
+                response.getConversionMultiple(),
+                quantity,
+                (quantity.multiply(response.getConversionMultiple())),
+                response.getPort());
     }
 }
